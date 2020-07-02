b293fae31e8add08cb1b0b3bef4af8dc4190a14b
Patricio <pat.floresa@alumnos.duoc.cl>
2020-07-02 00:45:21 -0400

    env

--- env_ferreteriaw/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\llano\desktop\ferme\ferreteria\env_ferreteriaw\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

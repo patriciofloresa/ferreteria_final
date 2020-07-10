4ca4d47add73fbe788e0eae14a058352cbdcb9fa
Patricio <pat.floresa@alumnos.duoc.cl>
2020-07-10 19:17:35 -0400

    DB to oracle

--- apps/abastecimiento/urls.py
+++ apps/abastecimiento/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = "abastecimiento"
 
 urlpatterns = [
-    path('pedido/list', views.PedidoListView.as_view(), name='pedido_list'),
-    path('pedido/add', views.PedidoAddView.as_view(), name='pedido_add'),
-    path('pedido/edit/<int:pk>/', views.PedidoEditView.as_view(), name='pedido_edit'),
-    path('pedido/delete/<int:pk>/', views.PedidoDeleteView.as_view(), name='pedido_delete'),
-    path('pedido/ver/<int:pk>/', views.PedidoShowView.as_view(), name='pedido_detail'),
-    path('pedido/list_pendientes', views.PedidoListPendientesView.as_view(), name='pedido_list_pendientes'),
+    # path('pedido/list', views.PedidoListView.as_view(), name='pedido_list'),
+    # path('pedido/add', views.PedidoAddView.as_view(), name='pedido_add'),
+    # path('pedido/edit/<int:pk>/', views.PedidoEditView.as_view(), name='pedido_edit'),
+    # path('pedido/delete/<int:pk>/', views.PedidoDeleteView.as_view(), name='pedido_delete'),
+    # path('pedido/ver/<int:pk>/', views.PedidoShowView.as_view(), name='pedido_detail'),
+    # path('pedido/list_pendientes', views.PedidoListPendientesView.as_view(), name='pedido_list_pendientes'),
 ]
